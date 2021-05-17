eba09d67baf394596b6b6037d18810423093ff86
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-05-17 13:42:41 -0400

    Add files via upload

---
+++ fontManipulator/main.js
@@ -0,0 +1,20 @@
+
+
+ function setup() {
+  video = createCapture(VIDEO,);
+  video.size(550, 550);
+   video.position(300,100)
+  canvas = createCanvas(550, 450);
+  canvas.position(1000,160);
+  
+  
+}
+
+
+
+
+
+function draw() {
+background('#dbc4ff');
+
+}
