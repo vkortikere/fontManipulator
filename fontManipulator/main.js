46fcbc183e15576571678dd612b33d106a76de55
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-06-05 13:45:17 -0400

    Add files via upload

--- fontManipulator/main.js
+++ fontManipulator/main.js
@@ -1,15 +1,25 @@
 
+noseX=0;
+noseY=0;
+difference=0;
+leftWristX=0;
+RightWristX=0;
 
 function setup(){
     video = createCapture(VIDEO);
-    video.size(550,500);
-    canvas = createCanvas(550,550);
-    canvas.position(560,150);
+    video.size(550,550);
+    canvas = createCanvas(700,550);
+    canvas.position(1060,150);
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
+    video.position(300,150)
 }
 function draw(){
-    background('#96#5500ff');
+background('#d6b0ff');
+fill('#03b1fc');
+stroke('#03b1fc');
+text("Vaishnavi", noseX, noseY);
+textSize(difference);
 }
 function modelLoaded(){
     console.log('posenet is initialized');
@@ -17,6 +27,11 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-      
+        noseX = results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y; 
+        leftWristX= results[0].pose.leftWrist.x ;
+        rightWristX= results[0].pose.rightWrist.x ;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX +  "difference = "+ difference)
   }
 }
